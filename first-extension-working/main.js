14d1f6d86a733775f583b9636cfd416ceebbcee3
Jeff Jacob <phansoft@gmail.com>
2018-09-09 12:38:30 -0700

    Changed working example to hover. Removed twitter, added bonus sample

--- first-extension-working/main.js
+++ first-extension-working/main.js
@@ -1,8 +1,16 @@
 var allImages = document.getElementsByTagName('img');
 console.log(allImages);
 for (var i = 0; i < allImages.length; i++) {
+    allImages[i].addEventListener('mouseenter',function() {
+        var imageHeight = this.clientHeight;
+        var imageWidth = this.clientWidth;
+        this.setAttribute('data-originalsrc', this.getAttribute('src'));
+        this.setAttribute('src', 'https://placekitten.com/' + imageWidth + '/' + imageHeight);
+    });
+    allImages[i].addEventListener('mouseleave',function() {
+        this.setAttribute('src', this.getAttribute('data-originalsrc'));
+    });
+    
     console.log(allImages[i]);
-    var imageHeight = allImages[i].clientHeight;
-    var imageWidth = allImages[i].clientWidth;
-    allImages[i].setAttribute('src', 'https://placekitten.com/' + imageWidth + '/' + imageHeight);
+    
 }
